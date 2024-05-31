18250208c17345d2ecb5343dc082db59fd7ad5aa
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-31 14:12:09 +0400

    feat: add elementary operations

---
+++ one.py
@@ -0,0 +1,37 @@
+# print("Hello, World")
+
+
+# num = 4
+# print(num)
+
+
+# num1 = 4
+# num2 = 2
+# print(num1 + num2)
+
+
+# print(num1-num2)
+
+
+# print(num1*num2)
+
+
+# print(num1/num2)
+
+
+# print(num1%num2)
+
+
+# str = "Hello, World"
+# print(len(str))
+
+
+# str1 = "Hello, "
+# str2 = "World"
+# print(str1+str2)
+
+
+# print(str1.upper())
+
+
+# print(str2.lower())
