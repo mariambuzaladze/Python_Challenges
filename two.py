8a5245adea72994d5ead14aac259dbfe00e25e3d
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-31 14:14:13 +0400

    feat: add simple functions

---
+++ two.py
@@ -0,0 +1,119 @@
+# N1
+# def greeting():
+#     print("Hello")
+
+# greeting()
+
+# N2
+# def sum(num1,num2):
+#     return num1+num2
+
+# print(sum(4,3))
+
+# N3
+# def length(str):
+#     return len(str)
+
+# print(length("Hello, World"))
+
+# N4
+# def concat(str1,str2):
+#     return str1+str2
+
+# print(concat("Bit", "Camp"))
+
+# N5
+# def number(num):
+#     return num
+
+# my_num = number(4)
+# print(my_num)
+
+# N6
+# def upper_case(str):
+#     return str.upper()
+
+# print(upper_case("hello"))
+
+# N7
+# def lower_case(str):
+#     return str.lower()
+
+# print(lower_case("HELLO"))
+
+# N8
+# def modulus(num1,num2):
+#     return num1%num2
+
+# print(modulus(5,4))
+
+# N9
+# def greeting(name):
+#     return f"Hello, {name}"
+
+# name = "Mariam"
+# print(greeting(name))
+
+# N10
+# def subtract(num1,num2):
+#     return num1-num2
+
+# print(subtract(8,4))
+
+# N11
+# def even(num):
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# print(even(5))
+
+# N12
+# def nums(num):
+#     if num > 0:
+#         return "positive"
+#     elif num < 0:
+#         return "negatve"
+#     else:
+#         return "zero"
+    
+# print(nums(4))
+
+# N13
+# def devidable(num1,num2):
+#     if num1%num2 == 0 :
+#         return "devidable"
+#     else:
+#         return "not devidable"
+    
+# print(devidable(8,4))
+
+# N14
+# def empty(str):
+#     if len(str) == 0 :
+#         return True
+#     else :
+#         return False
+    
+# print(empty(""))
+
+# N15
+# def includes(str,word):
+#     if word in str:
+#         return True
+#     else :
+#         return False
+    
+# print(includes("Hello, World","Hello"))
+
+# N16
+# def greater(num1,num2):
+#     if num1>num2 :
+#         return "First is greater"
+#     elif num2>num1 :
+#         return "Second is greater"
+#     else :
+#         return "They are equal"
+    
+# print(greater(4,2))
